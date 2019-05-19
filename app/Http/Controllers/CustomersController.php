51d13ceaa4e8e00f56fa6794d46351f20326d6cf
Sinh <sinh.buidinh@gmail.com>
2019-05-19 18:26:07 +0700

    scope: 2/customer init get 200 record by pagination 15, benchmark: queries: 17, memory_usage: 10.58MB, duration: 200ms, v7.1.22
    select count(*) as aggregate from `customers`
    select * from `customers` limit 15 offset 0
    select * from `companies` where `companies`.`id` = ? limit 1 (15 query)

---
+++ app/Http/Controllers/CustomersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function index(Request $request)
+    {
+        $customers = Customer::paginate();
+
+        return view('scope.customers', ['customers' => $customers]);
+    }
+}
---
+++ resources/views/scope/customers.blade.php
@@ -0,0 +1,36 @@
+@extends('layout', ['title' => 'Customers'])
+
+@section('content')
+
+<h1>Customers <small class="text-muted font-weight-light">({{ number_format($customers->total()) }} found)</small></h1>
+
+<form class="input-group my-4" action="{{ route('customers') }}" method="get">
+    <input type="hidden" name="order" value="{{ request('order') }}">
+    <input type="text" class="w-50 form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
+    <select name="filter" class="custom-select">
+        <option value="">Filters...</option>
+        <option value="birthday_this_week" {{ request('filter') === 'birthday_this_week' ? 'selected' : '' }}>Birthday this week</option>
+    </select>
+    <div class="input-group-append">
+        <button class="btn btn-primary" type="submit">Search</button>
+    </div>
+</form>
+
+<table class="table my-4">
+    <tr>
+        <th><a>Name</a></th>
+        <th><a>Company</a></th>
+        <th><a>Birthday</a></th>
+    </tr>
+    @foreach ($customers as $customer)
+        <tr>
+            <td><a>{{ $customer->last_name }}, {{ $customer->first_name }}</a></td>
+            <td>{{ $customer->companyName }}</td>
+            <td>{{ $customer->birth_date->format('F j') }}</td>
+        </tr>
+    @endforeach
+</table>
+
+{{ $customers->appends(request()->all())->links() }}
+
+@endsection
